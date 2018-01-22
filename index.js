cd1238a06b2de6313a682b1b6fd94c1cfdcd75b3
Christine Prevel <christine.prevel@gmail.com>
2018-01-22 19:44:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function logWhisper (string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) {
   console.log("I can't hear you!");
-  else if (string === string.toUpperCase) {
+ } else if (string === string.toUpperCase) {
   return "YES INDEED!";
-}}}
+}}
