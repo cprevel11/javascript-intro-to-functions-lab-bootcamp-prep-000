2a63b8e5d645f85eb65ba0c282ea3d4b22ef37b7
Christine Prevel <christine.prevel@gmail.com>
2018-01-22 19:45:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,7 @@ function sayHiToGrandma(string) {
   console.log("I can't hear you!");
  } else if (string === string.toUpperCase) {
   return "YES INDEED!";
-}}
+ } else if (string === "I love you, Grandma") {
+  return "I love you too" 
+ }
+}
